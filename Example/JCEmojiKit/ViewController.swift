7c5a5e586f81dd236780c13cccd096657fcb1d27
zk <zk1947@163.com>
2024-07-04 13:44:56 +0800

    Initial commit

---
+++ Example/JCEmojiKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JCEmojiKit
+//
+//  Created by zk1947@163.com on 07/04/2024.
+//  Copyright (c) 2024 zk1947@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
